c784a3b94f964192eb3fad250db9ccb2960d2610
doohong <wnghd94@gmail.com>
2021-08-12 23:37:15 +0900

    rabitmq cloud bus 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-config")
     implementation("org.springframework.cloud:spring-cloud-starter-bootstrap")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.cloud:spring-cloud-starter-bus-amqp")
     //security
     api("org.springframework.boot:spring-boot-starter-security")
     //mapper
